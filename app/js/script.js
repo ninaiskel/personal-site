837140d4a08862e5b05fc4d60e39511ff5b0f1ac
Jessica Aristimuño <ninaiskel@icloud.com>
2020-03-31 22:42:08 +0200

    last adjustments

--- app/js/script.js
+++ app/js/script.js
@@ -12,4 +12,12 @@ toggleSwitch.addEventListener("change", switchTheme);
 
 // projects
 
-const projects = document.querySelectorAll(".project");
+// const container = document.querySelector(".container");
+// const project = document.querySelector(".project");
+
+// const prespectiveProject = function(e) {
+//   console.log(project.offsetWidth, project.offsetHeight);
+//   console.log(e.clientX, e.clientY);
+// };
+
+// project.addEventListener("mouseover", prespectiveProject);
